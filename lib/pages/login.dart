9bd98dd5b9a0aa41ebdeea45bb9e17aa671391c0
vijeth_simha <vijethsimha71@gmail.com>
2023-09-13 00:11:28 +0530

    playing around containers

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -56,26 +56,62 @@ class _LoginState extends State<Login> {
                   child: SingleChildScrollView(
                     child: Padding(
                       padding: const EdgeInsets.all(30),
-                      child: Form(
-                          key: formKey,
-                          child: Column(
-                            children: [
-                              TextFormField(
-                                controller: emailController,
-                                decoration: const InputDecoration(
-                                    labelText: "Enter your email"),
-                                validator: (value) {
-                                  if (value!.isEmpty ||
-                                      !RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
-                                          .hasMatch(value)) {
-                                    return "Enter the email correctly";
-                                  } else {
-                                    return null;
-                                  }
-                                },
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 13.0, // soften the shadow
+                                spreadRadius: 5.0, //extend the shadow
+                                offset: Offset(
+                                  5.0, // Move to right 5  horizontally
+                                  5.0, // Move to bottom 5 Vertically
+                                ),
                               )
                             ],
-                          )),
+                            color: Colors.white,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20.0))),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 20),
+                          child: Form(
+                              key: formKey,
+                              child: Column(
+                                children: [
+                                  TextFormField(
+                                    controller: emailController,
+                                    decoration: const InputDecoration(
+                                        labelText: "Enter your email"),
+                                    validator: (value) {
+                                      if (value!.isEmpty ||
+                                          !RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+                                              .hasMatch(value)) {
+                                        return "Enter the email correctly";
+                                      } else {
+                                        return null;
+                                      }
+                                    },
+                                  ),
+                                  const SizedBox(height: 20),
+                                  TextFormField(
+                                    controller: emailController,
+                                    decoration: const InputDecoration(
+                                        labelText: "Enter your password"),
+                                    validator: (value) {
+                                      if (value!.isEmpty ||
+                                          !RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+                                              .hasMatch(value)) {
+                                        return "Enter the email correctly";
+                                      } else {
+                                        return null;
+                                      }
+                                    },
+                                  ),
+                                ],
+                              )),
+                        ),
+                      ),
                     ),
                   ),
                 ),
