5e63e416202a19536ce73b88e7bbf813bcceaf02
vijeth_simha <vijethsimha71@gmail.com>
2023-09-12 23:59:24 +0530

    added a container with text field

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -17,33 +17,69 @@ class _LoginState extends State<Login> {
           width: double.infinity,
           decoration: BoxDecoration(
               gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-            Colors.orange[900]!,
-            Colors.orange[800]!,
-            Colors.orange[400]!
+            Colors.blue[900]!,
+            Colors.blue[800]!,
+            Colors.blue[400]!
           ])),
           child: Column(
-            children: [
-              const Text("Login"),
-              Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: emailController,
-                        decoration: const InputDecoration(
-                            labelText: "Enter your email"),
-                        validator: (value) {
-                          if (value!.isEmpty ||
-                              !RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
-                                  .hasMatch(value)) {
-                            return "Enter the email correctly";
-                          } else {
-                            return null;
-                          }
-                        },
-                      )
-                    ],
-                  ))
+            children: <Widget>[
+              const SizedBox(
+                height: 80,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const <Widget>[
+                    Text(
+                      "Login",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Welcome Back",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(60),
+                          topRight: Radius.circular(60))),
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: Form(
+                          key: formKey,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                controller: emailController,
+                                decoration: const InputDecoration(
+                                    labelText: "Enter your email"),
+                                validator: (value) {
+                                  if (value!.isEmpty ||
+                                      !RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+                                          .hasMatch(value)) {
+                                    return "Enter the email correctly";
+                                  } else {
+                                    return null;
+                                  }
+                                },
+                              )
+                            ],
+                          )),
+                    ),
+                  ),
+                ),
+              )
             ],
           )),
     );
