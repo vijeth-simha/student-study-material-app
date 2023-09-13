7727fc53a0a5db50c7f9651c382eb060beed97ea
vijeth_simha <vijethsimha71@gmail.com>
2023-09-14 00:47:08 +0530

    signup page started

---
+++ lib/pages/singup.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class Signup extends StatefulWidget {
+  const Signup({Key? key}) : super(key: key);
+
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Signup"),
+        centerTitle: true,
+      ),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+          Colors.blue[900]!,
+          Colors.blue[800]!,
+          Colors.blue[400]!
+        ])),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 80,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const <Widget>[
+                  Text(
+                    "Signup",
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Thank you for your interest",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
