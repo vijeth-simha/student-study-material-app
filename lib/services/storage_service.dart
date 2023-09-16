aff5071c37483329b41ea86bd62b9f53568c55c8
vijeth_simha <vijethsimha71@gmail.com>
2023-09-17 00:37:32 +0530

    storage service method is added

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -2,7 +2,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:student_study_material/models/storage_items.dart';
 
 class StorageService {
-  final _secureStorage = FlutterSecureStorage();
+  final _secureStorage = const FlutterSecureStorage();
 
   AndroidOptions _getAndroidOptions() => const AndroidOptions(
         encryptedSharedPreferences: true,
@@ -17,5 +17,26 @@ class StorageService {
     var readData =
         await _secureStorage.read(key: key, aOptions: _getAndroidOptions());
     return readData;
+  }
+
+  Future<void> deleteSecureData(StorageItem item) async {
+    await _secureStorage.delete(key: item.key, aOptions: _getAndroidOptions());
+  }
+
+  Future<bool> containsKeyInSecureData(String key) async {
+    var containsKey = await _secureStorage.containsKey(
+        key: key, aOptions: _getAndroidOptions());
+    return containsKey;
+  }
+
+  Future<List<StorageItem>> readAllSecureData() async {
+    var allData = await _secureStorage.readAll(aOptions: _getAndroidOptions());
+    List<StorageItem> list =
+        allData.entries.map((e) => StorageItem(e.key, e.value)).toList();
+    return list;
+  }
+
+  Future<void> deleteAllSecureData() async {
+    await _secureStorage.deleteAll(aOptions: _getAndroidOptions());
   }
 }
