b6e7079dd7267f1b7e30c72702e7ac966c6e2478
vijeth_simha <vijethsimha71@gmail.com>
2023-09-20 00:27:21 +0530

    added sidebar meny

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -13,6 +13,42 @@ class _DashboardState extends State<Dashboard> {
     return Scaffold(
       appBar: AppBar(title: const Text('Dashboard')),
       body: const Text('Hello World'),
+      drawer: NavigationDrawer(),
     );
   }
+}
+
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            const Divider(color: Color.fromARGB(179, 243, 0, 0)),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      );
 }
