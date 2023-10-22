3ef8458c3a86dd174ed70c2aeab476693663d215
vijeth_simha <vijethsimha71@gmail.com>
2023-10-22 16:48:40 +0530

    semester added

--- lib/pages/semesters.dart
+++ lib/pages/semesters.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
-class SemesterPage extends StatelessWidget {
+class SemesterPage extends StatefulWidget {
   const SemesterPage({super.key});
 
   @override
+  State<SemesterPage> createState() => _SemesterPageState();
+}
+
+class _SemesterPageState extends State<SemesterPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Text("Semester page done");
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Semester"),
+      ),
+      body: GridView.builder(
+          scrollDirection: Axis.vertical,
+          physics: const BouncingScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, childAspectRatio: 1),
+          itemCount: 3,
+          itemBuilder: ((context, index) {
+            return const Row(
+              children: [Text("Semester")],
+            );
+          })),
+    );
   }
 }
