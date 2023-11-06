05191a6512cbe2d931e0201de00a48f11524f030
vijeth_simha <vijethsimha71@gmail.com>
2023-11-06 22:31:10 +0530

    pushed the code

--- lib/pages/documents.dart
+++ lib/pages/documents.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
 
 class DocumentsPage extends StatefulWidget {
   const DocumentsPage({super.key});
@@ -8,6 +9,11 @@ class DocumentsPage extends StatefulWidget {
 }
 
 class _DocumentsPageState extends State<DocumentsPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
