7a96afa4a47d7f9ff2ae680d9d35f9d7fd1ff01f
vijeth_simha <vijethsimha71@gmail.com>
2023-11-09 23:15:11 +0530

    added profile page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:student_study_material/pages/dashboard.dart';
 import 'package:student_study_material/pages/documents.dart';
 import 'package:student_study_material/pages/login.dart';
+import 'package:student_study_material/pages/profile.dart';
 import 'package:student_study_material/pages/semesters.dart';
 import 'package:student_study_material/pages/signup.dart';
 import 'package:student_study_material/pages/subjects.dart';
@@ -11,6 +12,7 @@ void main() {
     routes: {
       '/': (context) => const Login(),
       '/signup': (context) => const Signup(),
+      '/profile': (context) => const ProfilePage(),
       '/dashboard': (context) => const Dashboard(),
       '/semesters': (context) => const SemesterPage(),
       '/subjects': (context) => const SubjectsPage(),
